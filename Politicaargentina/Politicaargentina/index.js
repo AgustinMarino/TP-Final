6fd7d7f0923d74750188a0b13211e3abb4eb4e4c
AgustinMarino <agustinemarino@gmail.com>
2023-12-11 17:41:34 -0300

    manito

---
+++ Politicaargentina/Politicaargentina/index.js
@@ -0,0 +1,13 @@
+function politicos(params) {
+    let politicos = document.querySelector("#politicos");
+    let partidos = document.querySelector("#partidos");
+    partidos.style = "display: none";
+    politicos.style = "display: block";
+}
+
+function partidos(params) {
+    let politicos = document.querySelector("#politicos");
+    let partidos = document.querySelector("#partidos");
+    partidos.style = "display: block";
+    politicos.style = "display: none";
+}
--- Politicaargentina/Politicaargentina/politicaargentina.php
+++ Politicaargentina/Politicaargentina/politicaargentina.php
@@ -11,7 +11,7 @@ include("config/bd.php");?>
         <img class="card-img-top" src="holder.js/100x180/" alt="">
         <div class="card-body">
             <h4 class="card-title">Partidos politicos</h4>
-            <a name="" id="" class="btn btn-primary" href="$partidospoliticos" role="button">Ver mas</a>
+            <a onclick="partidos()" name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
         </div>
     </div>    
 
@@ -21,11 +21,11 @@ include("config/bd.php");?>
         <img class="card-img-top" src="holder.js/100x180/" alt="">
         <div class="card-body">
             <h4 class="card-title">Politicos</h4>
-       <a name="" id="" class="btn btn-primary" href="sentenciaSQL = $conexion->prepare(select * from partidospoliticos)" role="button">Ver mas</a>
+       <a onclick="politicos()" name="" id="" class="btn btn-primary" href="#" role="button">Ver mas</a>
         </div>
     </div>    
 
-    <table id="partidos" class="table table-bordered">
+    <table id="partidos" class="table table-bordered" style="display: none;">
 
     <?php
     $base = "politicaargentina";
@@ -60,7 +60,7 @@ include("config/bd.php");?>
     ?>
     </table>
 
-    <table id="politicos" class="table table-bordered">
+    <table id="politicos" class="table table-bordered" style="display: none;">
 
 <?php
 $base = "politicaargentina";
@@ -70,14 +70,16 @@ $consulta = mysqli_query($conexion, $consulta);
 
 echo '<thead>
     <tr>
+        <th>Politico_ID</th>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Cargo</th>
         <th>Partido_ID</th>
-        <th>NombrePartido</th>
-        <th>Ideologia</th>
-        <th>Aniofundacion</th>
-        <th>SedeCentral</th>
-        <th>LiderActual</th>
-        <th>SitioWeb</th>   
-    </tr>
+        <th>Educacion</th>
+        <th>Fecha de Nacimiento</th>   
+        <th>Biografia</th>
+        <th>Imagen</th>
+        </tr>
     </thead>';
 while($resultado = mysqli_fetch_row($consulta)){
     echo '<tr>
@@ -85,15 +87,17 @@ while($resultado = mysqli_fetch_row($consulta)){
         <td>'.$resultado[1].'</td>
         <td>'.$resultado[2].'</td>
         <td>'.$resultado[3].'</td>
-        <td>'.$resultado[5].'</td>
         <td>'.$resultado[4].'</td>
-        <td>'.$resultado[6].'</td>  
+        <td>'.$resultado[5].'</td>
+        <td>'.$resultado[6].'</td>
+        <td>'.$resultado[7].'</td>
+        <td>'.$resultado[8].'</td>  
     </tr>';
 }
 echo '</tbody>';
 
 ?>
 </table>
-
+<script src="index.js"></script>
 
 <?php include('template/pie.php'); ?>
